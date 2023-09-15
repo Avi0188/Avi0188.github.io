9bdf0c29fbe3da069538aa4128152b0f05a54bd4
unknown <rai2889@gmail.com>
2023-09-16 01:38:59 +0530

    prot1

---
+++ index.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+      if(this.scrollY > 20){
+          $('#main-nav').addClass("sticky");
+      }else{
+          $('#main-nav').removeClass("sticky");
+      }
+      
+  })
+
+  // togel/menu nevbar script
+  $('.menu-btn').click(function(){
+      $('#main-nav .menu').toggleClass("active");
+      $('.menu-btn i').toggleClass("active");
+  });
+
+  $('.menu li a').click(function(){
+      $('#main-nav .menu').toggleClass("active");
+      $('.menu-btn i').toggleClass("active");
+  });
+
+  //   typing animation script 
+     var typed = new Typed (".panku",{
+      strings:["Full-Stack Web-Developer", "Frontend Devloper"],
+      typeSpeed:100,
+      backSpeed:60,
+      loop:true
+     })
+
+
+     var typed = new Typed (".panku2",{
+      strings:["Full-Stack Web-Developer", "Frontend Devloper"],
+      typeSpeed:100,
+      backSpeed:60,
+      loop:true
+     });
+
+  // owl carousel script
+  $('.carousel').owlCarousel({
+       margin:20,
+       loop:true,
+       autoplayTimeOut:2000,
+       autoplayHoverPause:true,
+       responsive:{
+          0:{
+              items:1,
+              nav:false
+          },
+          600:{
+              items:2,
+              nav:false
+          },
+          1000:{
+              items:3,
+              nav:false
+          }
+       }
+  })
+
+});
+
+// resume section 
+
+document.getElementById("resume-link-1").onclick = () => {
+  window.open(
+    "https://drive.google.com/file/d/1dipXkaaT1jQBbob6-OvZo0U5xSPRLGHa/view?usp=sharing"
+  );
+};
+
+document.getElementById("resume-link-2").onclick = () => {
+  window.open(
+    "https://drive.google.com/file/d/1dipXkaaT1jQBbob6-OvZo0U5xSPRLGHa/view?usp=sharing"
+  );
+};
---
+++ mail.js
@@ -0,0 +1,57 @@
+function SendMSG(e) {
+    e.preventDefault();
+    let nam_txt = document.querySelector("#name");
+    let email_txt = document.querySelector("#email");
+    let subject_txt = document.querySelector("#subject");
+    let message_txt = document.querySelector("#message");
+    let body =
+      "Name:" +
+      nam_txt.value +
+      ",  " +
+      "email:" +
+      email_txt.value +
+      ",  " +
+      "subject:" +
+      subject_txt.value +
+      ",  " +
+      "Message : " +
+      message_txt.value;
+  
+    let temp = {
+      from_name: nam_txt.value,
+      to_name: "Abhishek Rai",
+      message: body,
+    };
+    
+    emailjs.send("service_pjvggi9","template_du3j52c", temp).then((res) => {
+      if (res.status == 200) {
+        success();
+      } else {
+        failure();
+      }
+    });
+  
+    nam_txt.value = "";
+    email_txt.value = "";
+    subject_txt.value = "";
+    message_txt.value = "";
+  }
+  
+  function success() {
+    // let thank = document.querySelector(".msg-pop-up");
+    // thank.style.display = "block";
+    // setTimeout(() => {
+    //   thank.style.display = "none";
+    // }, 2000);
+    alert("Mail sent successfully!")
+  }
+  
+  function failure() {
+    // let fail = document.querySelector(".failed-msgpop");
+    // fail.style.display = "block";
+    // setTimeout(() => {
+    //   fail.style.display = "none";
+    // }, 2000);
+    alert ("message failed")
+  }
+  
